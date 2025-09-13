9c949e5fb28ae059cf15ba891bd53b8f99c0ae4e
vimto1234 <dvd.thwaites@gmail.com>
2025-09-13 22:28:17 +0100

    Add player movement

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+
+        #draw game
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = (game_clock.tick(60)/100)
 
 if __name__ == "__main__":
     main()
